9aafee58a0076faf5fe526aba14ca932a97ead62
filip-codes <filip@bitecorn.com>
2024-06-01 18:27:07 +0200

    added post method

--- Studio.Foundation/Studio.Foundation/Application.cs
+++ Studio.Foundation/Studio.Foundation/Application.cs
@@ -46,6 +46,20 @@ public class Application
         Route route = new Route(path, controller, method, HttpMethod.Get);
         this._routes.Add(route);
     }
+    
+    public void Post(string path, string controller, string method)
+    {
+        this.Post(path, Type.GetType(controller + ", " + Assembly.GetEntryAssembly()?.GetName().Name), method);
+    }
+
+    public void Post(string path, Type? controller, string method)
+    {
+        if (controller is null)
+            throw new InvalidOperationException("Controller not found");
+
+        Route route = new Route(path, controller, method, HttpMethod.Post);
+        this._routes.Add(route);
+    }
 
     public string? ExecuteRoute(string path)
     {
