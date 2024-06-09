2890202a6fe7a35d47b5c5613e0889572f2efaf7
filip-codes <filip@bitecorn.com>
2024-06-09 10:11:44 +0200

    remove UserController

--- Routes/Api.cs
+++ Routes/Api.cs
@@ -1,11 +1,9 @@
-using Studio.App.Http.Controllers.Api;
-
 namespace Studio.Routes;
 
 public class Api : Foundation.Routes
 {
     public void Register()
     {
-        Route.Get("/users", typeof(UserController), "Index");
+        
     }
 }
